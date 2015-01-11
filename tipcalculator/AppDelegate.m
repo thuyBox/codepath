2367ebb715d2c1e19849c18a99bc626d038e96cc
Baeksan <baeksan@gmail.com>
2015-01-11 14:23:04 -0800

    remove Main.storyboard

--- tipcalculator/AppDelegate.m
+++ tipcalculator/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     TipViewController * vc = [[TipViewController alloc] init];
     UINavigationController* nvc = [[UINavigationController alloc] initWithRootViewController:vc];
